47d50380e5ccadf4f0c638ac9a482d8590d4f0a4
paplauskis <paplauskaskostas@gmail.com>
2023-06-03 14:23:55 +0300

    changed pen to paint only when mouse is clicked, added function to clear board, to select dimensions with slider and to change color with input

--- script.js
+++ script.js
@@ -1,35 +1,61 @@
 const containerDiv = document.querySelector('#container');
-const squareAmountButton = document.querySelector('#dimensions')
-squareAmountButton.addEventListener('click', removeGrid)
-squareAmountButton.addEventListener('click', gridSelection)
-
-createGrid(50)
+const buttonsDiv = document.querySelector('.buttons')
+const clearButton = document.querySelector('.clear');
+const penColor = document.querySelector('#pen-color');
+const squareDiv = document.querySelector('.square');
+clearButton.addEventListener('click', clearSquares);
+let currentColor = '#000000';
+let isMouseDown = false;
 
 function createGrid(gridSize) {
     for (i = 0; i < gridSize; i++) {
         for (j = 0; j < gridSize; j++) {
         let squareDiv = document.createElement('div');
-        squareDiv.style.width = `${800 / gridSize}px`;
-        squareDiv.style.height = `${800 / gridSize}}px`;
-        squareDiv.className = 'square'
-        containerDiv.appendChild(squareDiv)
+        squareDiv.style.width = `${700 / gridSize}px`;
+        squareDiv.style.height = `${700 / gridSize}}px`;
+        squareDiv.className = 'square';
+        containerDiv.appendChild(squareDiv);
+        squareDiv.addEventListener('mousedown', () => isMouseDown = true)
+        squareDiv.addEventListener('mouseup', () => isMouseDown = false)
         squareDiv.addEventListener('mouseover', () => {
-            squareDiv.style.backgroundColor = "lightblue";
+            if(isMouseDown) squareDiv.style.backgroundColor = currentColor;
         })
         }
     }
 }
 
-function gridSelection() {
-    let selectDimensions = prompt('Select how many squares you want in one direction (max value: 100)', '1')
-    if (selectDimensions > 100) {
-        alert('Error: maximum input is 100')
-    } else createGrid(selectDimensions);   
-    }
+createGrid(50);
+
+penColor.addEventListener('input', (e) => {
+    currentColor = e.target.value;
+});
 
 function removeGrid() {
-        let squares = containerDiv.querySelectorAll('.square');
-        squares.forEach((square) => {
-          containerDiv.removeChild(square);
-        });
+    let squares = containerDiv.querySelectorAll('.square');
+    squares.forEach((square) => {
+      containerDiv.removeChild(square);
+    });
 }
+
+function clearSquares() {
+    let squares = containerDiv.querySelectorAll('.square');
+    squares.forEach((square) => {
+      square.style.backgroundColor = 'white';
+    });
+}
+
+const dimensionsInput = document.getElementById('dimensions-input');
+const value = document.getElementById('value');
+dimensionsInput.addEventListener('input', updateInputValue);
+value.textContent = '50 x 50';
+value.style.margin = 'auto';
+
+function updateInputValue() {
+    value.textContent = `${dimensionsInput.value} x ${dimensionsInput.value}`;
+    removeGrid();
+    createGrid(dimensionsInput.value);
+}
+
+
+
+
